f56c7eea726cee8746f081132108a5ed7dcf00c2
root <root@localhost.localdomain>
2013-07-06 10:51:42 +0000

    chefsolo実行用にもろもろ修正

---
+++ README.md
@@ -0,0 +1,9 @@
+履歴
+========
+2013/7/6 ひとまずレシピ作成
+
+実行方法
+========
+solo.rb、chefsolo.jsonだけパスを通せば下記でchef-solo実行<br />
+sudo chef-solo -c solo.rb -j ./chefsolo.json
+
--- solo.rb
+++ solo.rb
@@ -1,4 +1,4 @@
 # solo.rb
 # cookbook_pathとかもう少しうまく書きたい..
 file_cache_path "/tmp/chef-solo"
-cookbook_path ["/home/vagrant/appServer/cookbooks"]
+cookbook_path ["/home/vagrant/phpServer/cookbooks"]
